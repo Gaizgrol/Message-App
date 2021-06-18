073495477cfe57aa91b46ccc1fbf49d2a66c7b75
Gaizgrol <gabrielizotongo@gmail.com>
2021-06-18 19:38:46 -0300

    +Implementação criação de mensagens

--- src/message-app-api/http/controllers/messages.js
+++ src/message-app-api/http/controllers/messages.js
@@ -1,11 +1,20 @@
 const db = require( '../../database/models/' );
+const { Message } = db;
 
 class MessagesController
 {
     static async create( req, res )
     {
-        const message = 'Salve!';
-    
+        // Busca dados da request
+        const { chatId, content, userId } = { ...req.body };
+
+        // Valida
+        if ( !chatId || !content || !userId )
+            return res.status( 400 ).json({ error: 'Um dos campos da requisição está faltando!' });
+
+        // Cria a mensagem
+        const message = await Message.create({ chatId, content, userId });
+        
         res.status( 201 ).json({ message });
     }
 }
